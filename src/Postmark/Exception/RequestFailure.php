39770c8633eb9f181ba8a3471d02054d0324dafb
George Steel <george@net-glue.co.uk>
2024-01-22 22:29:41 +0000

    Fix `RiskyTruthyFalsyComparison` issues

--- src/Postmark/Exception/RequestFailure.php
+++ src/Postmark/Exception/RequestFailure.php
@@ -23,39 +23,32 @@ final class RequestFailure extends RuntimeException implements PostmarkException
 
     public static function with(RequestInterface $request, ResponseInterface $response): self
     {
-        switch ($response->getStatusCode()) {
-            case 401:
-                return new self(
-                    'Unauthorized: Missing or incorrect API token in header. '
-                    . 'Please verify that you used the correct token when you constructed your client.',
-                    $request,
-                    $response,
-                );
-
-            case 500:
-                return new self(
-                    'Internal Server Error: This is an issue with Postmark’s servers processing your request. '
-                    . 'In most cases the message is lost during the process, '
-                    . 'and Postmark is notified so that we can investigate the issue.',
-                    $request,
-                    $response,
-                );
-
-            case 503:
-                return new self(
-                    'The Postmark API is currently unavailable, please try your request later.',
-                    $request,
-                    $response,
-                );
-
-            default:
-                return new self(
-                    self::retrieveErrorMessage($response)
-                        ?: 'A request error occurred and there was no message encoded in the response.',
-                    $request,
-                    $response,
-                );
-        }
+        return match ($response->getStatusCode()) {
+            401 => new self(
+                'Unauthorized: Missing or incorrect API token in header. '
+                . 'Please verify that you used the correct token when you constructed your client.',
+                $request,
+                $response,
+            ),
+            500 => new self(
+                'Internal Server Error: This is an issue with Postmark’s servers processing your request. '
+                . 'In most cases the message is lost during the process, '
+                . 'and Postmark is notified so that we can investigate the issue.',
+                $request,
+                $response,
+            ),
+            503 => new self(
+                'The Postmark API is currently unavailable, please try your request later.',
+                $request,
+                $response,
+            ),
+            default => new self(
+                self::retrieveErrorMessage($response)
+                    ?? 'A request error occurred and there was no message encoded in the response.',
+                $request,
+                $response,
+            ),
+        };
     }
 
     public function request(): RequestInterface
@@ -77,11 +70,10 @@ final class RequestFailure extends RuntimeException implements PostmarkException
     public function postmarkErrorCode(): int|null
     {
         $body = self::responseBody($this->response);
-        $code = isset($body['ErrorCode']) && is_numeric($body['ErrorCode']) && ! empty($body['ErrorCode'])
-            ? $body['ErrorCode']
-            : null;
 
-        return $code ? (int) $code : null;
+        return isset($body['ErrorCode']) && is_numeric($body['ErrorCode'])
+            ? (int) $body['ErrorCode']
+            : null;
     }
 
     /** @return non-empty-string|null */
